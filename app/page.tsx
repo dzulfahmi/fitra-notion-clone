039e0c248ed95b132802708fd381ac88b8070056
dzulfahmi <fitradzulfahmi@gmail.com>
2024-05-12 12:15:19 +0700

    init page and test page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <p>Hello notion clone</p>
+    <div>
+      <p className="text-red-500 font-bold">Hello notion clone</p>
+      <Button variant="outline">Click me</Button>
+    </div>
   );
 }
